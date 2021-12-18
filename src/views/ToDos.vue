14562d005a688a605da8ac961d97f40f3e54e875
Sinan1997 <Tomate1007>
2021-12-18 16:37:16 +0100

    ToDos gearbeitet

--- src/views/ToDos.vue
+++ src/views/ToDos.vue
@@ -1,6 +1,7 @@
 <template>
-  <h1>Willkommen auf deiner <span class="badge bg-secondary">To-Do Liste</span></h1>
+  <h1>Welcome to your To-Dos</h1>
 </template>
+  <h1>Willkommen auf deiner <span class="badge bg-secondary">To-Do Liste</span></h1>
 
 <script>
 export default {
