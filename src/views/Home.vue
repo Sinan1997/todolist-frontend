91b7dcbc73bfd48eb4b21376a7bbbe018e197bbf
Sinan1997 <Tomate1007>
2021-12-18 14:36:36 +0100

    Home Text gearbeitet

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Hello World!"/>
+    <HelloWorld msg="Willkommen auf deiner To-Do List. Auf dieser Seite kannst du To-Dos erstellen, abhaken oder löschen"/>
   </div>
 </template>
 
