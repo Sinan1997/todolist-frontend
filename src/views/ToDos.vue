71a4b8cace7af3e31fea6bd4e001645073caf4da
Sinan1997 <Tomate1007>
2021-12-19 14:41:38 +0100

    picture

--- src/views/ToDos.vue
+++ src/views/ToDos.vue
@@ -1,38 +1,13 @@
-<div class="row row-cols-1 row-cols-md-2 g-4">
-<div class="col">
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Max Mustermann</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
-  </div>
-</div>
-<div class="col">
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
-  </div>
-</div>
-<div class="col">
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-    </div>
-  </div>
-</div>
-<div class="col">
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
-  </div>
-</div>
-</div>
+<template>
+  <h1>Welcome to your To-Dos</h1>
+</template>
+
+<script>
+export default {
+  name: 'To-Dos'
+}
+</script>
+
+<style scoped>
+
+</style>
