20440e0e678a6a915c40f80cc238e4d8f7fa7120
Sinan1997 <Tomate1007>
2022-01-06 17:27:22 +0100

    Delete Test2

--- src/components/toDoCard.vue
+++ src/components/toDoCard.vue
@@ -45,6 +45,7 @@ export default {
         redirect: 'follow'
       }
       fetch(endpoint, requestOptions)
+        .then(response => response.text())
         .then(result => console.log(result))
         .catch(error => console.log('error', error))
     }
