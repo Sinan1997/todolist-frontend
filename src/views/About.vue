d825dbebf81af88edfe193cefbca88b3d392ea95
Sinan1997 <Tomate1007>
2022-01-09 14:55:13 +0100

    Design angepasst

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="about">
+    <P></P>
     <h1><span class="badge bg-secondary">About</span></h1>
     </div>
+  <P></P>
   <div class="aboutText">
     <h1>Diese Seite haben wir im Interesse der Nutzer erstellt. Sie soll Menschen im Alltag helfen, in dem Sie die Möglichkeit haben, ihre Aufgaben produktiv und effizient abzuarbeiten. </h1>
   </div>
